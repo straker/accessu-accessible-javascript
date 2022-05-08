602ef77b9ef9f7e24b37a607092d6c39d380aaa3
Steven Lambert <steven.lambert@deque.com>
2022-05-08 15:18:38 -0600

    typo

--- vue/cypress/integration/integration_spec.js
+++ vue/cypress/integration/integration_spec.js
@@ -1,10 +1,10 @@
-it('should navigate to the correct page' () => {
+it('should navigate to the correct page', () => {
   cy.visit('http://localhost:8080/');
   cy.title().should('eq', 'foo Social Platform')
 });
 
 describe('Accessibility', () => {
-  it('should have 0 axe violations' () => {
+  it('should have 0 axe violations', () => {
     cy.visit('http://localhost:8080/');
     cy.injectAxe();
     cy.checkA11y();
